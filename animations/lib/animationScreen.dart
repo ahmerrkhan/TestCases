bbc7f59ec4cc728fc444289a587ba63c813e1a00
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-05-05 00:13:31 +0000

    Update animationScreen.dart

--- animations/lib/animationScreen.dart
+++ animations/lib/animationScreen.dart
@@ -28,7 +28,7 @@ class _animationScreenState extends State<animationScreen> {
               width: _width,
               decoration: BoxDecoration(
                 color: _color,
-                borderRadius: BorderRadius.circular(15.0),
+                borderRadius: BorderRadius.circular(14.0),
               ),
       ),
          opacity: _opacity,
