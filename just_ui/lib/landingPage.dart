4b444abcd08f400cd1e9d7c76429adc44576c34c
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-05-22 09:19:18 +0500

    Update landingPage.dart

--- just_ui/lib/landingPage.dart
+++ just_ui/lib/landingPage.dart
@@ -14,7 +14,7 @@ class _landingPageState extends State<landingPage> {
       ),
       body: GridView.count(
         padding: EdgeInsets.all(10.0),
-        crossAxisCount: 3,
+        crossAxisCount: 22,
         mainAxisSpacing: 0.0,
         crossAxisSpacing: 0.0,
         children: [
