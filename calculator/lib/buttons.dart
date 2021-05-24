35bca7c59e5a7d090052e5144aa6b0e218f28158
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-05-24 10:47:18 +0500

    Update buttons.dart

--- calculator/lib/buttons.dart
+++ calculator/lib/buttons.dart
@@ -8,7 +8,7 @@ class buttons extends StatefulWidget {
 
 class _buttonsState extends State<buttons> {
 
-  int firstNum=;
+  int firstNum cupei o
   int secondNum;
   String textToDisplay = "";
   String res ;
